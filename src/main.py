160a47f39accf0a6ac2617962075aa3fee930dbc
Zakatin Stepan < >
2025-12-22 13:44:35 +0300

    add input

--- src/main.py
+++ src/main.py
@@ -7,9 +7,15 @@ def main() -> None:
     Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
     :return: Данная функция ничего не возвращает
     """
+    step = int(input("Введите кол-во шагов симуляции: "))
+    seed = input("Введите сид(или оставьте пустым): ")
+    if seed:
+        seed = int(seed)
+    else:
+        seed = None
     simulation = LibrarySimulation()
     AppLogger.configure_logger()
-    print(simulation.run_simulation(cst.STEPS, cst.SEED))
+    print(simulation.run_simulation(step, seed))
 
 
 if __name__ == "__main__":
